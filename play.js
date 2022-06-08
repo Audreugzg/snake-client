ae6bfb6a3aa8d33d76a1732987e704c40f6a1c3b
Zuge Gao <audreygaozu@hotmail.com>
2022-06-08 22:17:43 +0000

    input modoule

--- play.js
+++ play.js
@@ -1,29 +1,23 @@
 // const net = require("net");
 //const connect = require("./clients.js");
 const {conn,textEncoding,connData,connect,connect1} = require('./clients');
+const {stdin,setRawMode,setEncoding,resume,on} = require('./input')
 
 console.log("Connecting ...");
 
+//connect(); 
 conn;
 textEncoding;
 connData;
 connect;
 connect1;
 
-// setup interface to handle user input from stdin
-const handleUserInput = function (key) {
-  // your code here
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
+//setupInput();
+stdin;
+setRawMode;
+setEncoding;
+resume;
+on;
 
-setupInput();
+
+
