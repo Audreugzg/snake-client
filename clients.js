333e4471430a0005b9957b95e914bd749fe4f148
Zuge Gao <audreygaozu@hotmail.com>
2022-06-08 21:06:49 +0000

    snake initials

---
+++ clients.js
@@ -0,0 +1,42 @@
+// establishes a connection with the game server
+// const connect = function () {
+//   const conn = net.createConnection({
+//     host: '165.227.47.243' ,// IP address here,
+//     port: 50541,// PORT number here,
+//   });
+
+//   // interpret incoming data as text
+//   conn.setEncoding("utf8");
+
+//   conn.on("connect", () => {
+//     // code that does something when the connection is first established
+//     console.log("the connection is succussfully established");
+//   });
+//   conn.on("data", (data) => {
+//     // code that does something when the connection is first established
+//     console.log(data);
+//   });
+
+//   return conn;
+// };
+net = require("net");
+const conn = net.createConnection({
+  host: '165.227.47.243' ,// IP address here,
+  port: 50541,// PORT number here,
+});
+
+const textEncoding = conn.setEncoding("utf8");
+const connData = conn.on("data", (data) => {
+  // code that does something when the connection is first established
+  console.log(data);
+});
+
+const connect = conn.on("connect", (client) => {
+       // code that does something when the connection is first established
+       console.log("the connection is succussfully established");
+       conn.write("Name: AG");
+
+     });
+
+
+module.exports = {conn,textEncoding,connData,connect};
