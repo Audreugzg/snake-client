b4d240169415bb82ce52152be16b7b10acc740b1
Zuge Gao <audreygaozu@hotmail.com>
2022-06-08 23:54:43 +0000

    constants add

--- input.js
+++ input.js
@@ -1,29 +1,30 @@
 // setup interface to handle user input from stdin
 let connection;
+const { MOVE_UP_KEY,MOVE_DOWN_KEY,MOVE_LEFT_KEY,MOVE_RIGHT_KEY,Exit_KEY,MESSAGE_KEY} = require("./constants");
 const handleUserInput = function (key) {
   // your code here
-  if (key === '\u0003') {
+  if (key === Exit_KEY) {
     process.exit();
   }
-  if (key === 'w') {
+  if (key === MOVE_UP_KEY) {
     //console.log("Move: up");
     connection.write("Move: up");
   }
-  if (key === 'a') {
+  if (key === MOVE_LEFT_KEY) {
     //console.log("Move: left");
     connection.write("Move: left");
   }
-  if (key === 's') {
+  if (key === MOVE_DOWN_KEY) {
     //console.log("Move: down");
     connection.write("Move: down");
   }
-  if (key === 'd') {
+  if (key === MOVE_RIGHT_KEY) {
     //console.log("Move: right");
     connection.write("Move: right");
   }
-  if (key === 'm') {
+  if (MESSAGE_KEY[key]) {
     //console.log("Move: right");
-    connection.write("Say: HaHaHa 🐍🐍🐍🇨🇦🇨🇳");
+    connection.write(MESSAGE_KEY[key]);
   }
 };
 
